3704dc01a7fd65c18a56d625a30c41c82ea1026e
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-14 18:30:06 +0100

    02_08 Actualizar/agregar valores a una clave en Redis usando Node.js

---
+++ 02_08/app.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+
+const cliente_redis = createClient({
+    host: 'localhost',
+    port: 6379,
+    password: '123Queso.'
+});
+cliente_redis.on('error', function (error) {
+    console.log('Error ' + error);
+});
+
+await cliente_redis.connect();
+
+await cliente_redis.set('persona', 'Sofia');
