964b702c42f913fe6778050dc9a8c52b654cf02c
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-14 22:07:19 +0100

    03_04 Guardar y obtener pandas.DataFrame desde Redis con Python

---
+++ 03_04/app.py
@@ -0,0 +1,17 @@
+import redis
+import pandas as pd
+
+cliente_redis = redis.Redis(host="localhost", port=6379, db=0, password="123Queso.")
+
+df = pd.DataFrame({
+    'A': [1, 2, 3],
+    'B': [4, 5, 6],
+    'C': [7, 8, 9]
+})
+
+cliente_redis.set("dataframe", df.to_json())
+
+df_redis = cliente_redis.get("dataframe").decode("utf8")
+df_redis = pd.read_json(df_redis)
+
+print(df_redis)
