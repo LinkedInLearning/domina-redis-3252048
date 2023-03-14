901cca5cfe764f60cafaba0a04518155313099ad
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-14 16:33:18 +0100

    02_04 Obtener todas las claves de Redis usando PHP

---
+++ 02_04/app.php
@@ -0,0 +1,12 @@
+<?php
+$cliente_redis = new Redis();
+try {
+    $cliente_redis->connect('127.0.0.1', 6379);
+    $cliente_redis->auth('123Queso.');
+
+
+} catch (RedisException $error) {
+    echo "Error al conectarse a Redis: " . $error->getMessage();
+}
+
+?>
