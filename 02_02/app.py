92f232f02e9f1eb732356c0732db8c2510e99d01
Lincy Gonzalez Rojas <lincgr@gmail.com>
2023-03-14 15:24:36 +0100

    02_02 Cómo almacenar un diccionario en Redis con Python

---
+++ 02_02/app.py
@@ -0,0 +1,10 @@
+import redis
+
+redis_cliente = redis.Redis(host='localhost', port=6379, db=0, password="123Queso.")
+
+persona = {
+  "nombre": "Isabel",
+  "apellido": "Rojas",
+  "fecha_nacimiento": 1965
+}
+
